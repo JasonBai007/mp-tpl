3c9131df72d538a30cff6484f28b34415935876a
JasonBai007 <byygkcg@163.com>
2019-08-27 14:39:41 +0800

    优化逻辑

--- pages/charts/charts.js
+++ pages/charts/charts.js
@@ -1,102 +1,53 @@
 import * as echarts from '../../ec-canvas/echarts';
 
-let chart = null;
-
-function initChart(canvas, width, height) {
-  chart = echarts.init(canvas, null, {
-    width: width,
-    height: height
-  });
-  canvas.setChart(chart);
-
-  var symbolSize = 20;
-  var data = [
-    [15, 0],
-    [-50, 10],
-    [-56.5, 20],
-    [-46.5, 30],
-    [-22.1, 40]
-  ];
-
-  var option = {
-    title: {
-      text: 'Try Dragging these Points'
-    },
-    tooltip: {
-      triggerOn: 'none',
-      formatter: function(params) {
-        return 'X: ' + params.data[0].toFixed(2) + '<br>Y: ' + params.data[1].toFixed(2);
-      }
-    },
-    grid: {},
+function genOption() {
+  return {
+    color: ['#37a2da', '#32c5e9', '#67e0e3'],
     xAxis: {
-      min: -100,
-      max: 80,
-      type: 'value',
-      axisLine: {
-        onZero: false
-      }
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
     },
     yAxis: {
-      min: -30,
-      max: 60,
-      type: 'value',
-      axisLine: {
-        onZero: false
-      }
+      type: 'value'
     },
-    dataZoom: [{
-        type: 'slider',
-        xAxisIndex: 0,
-        filterMode: 'empty'
-      },
-      {
-        type: 'slider',
-        yAxisIndex: 0,
-        filterMode: 'empty'
-      },
-      {
-        type: 'inside',
-        xAxisIndex: 0,
-        filterMode: 'empty'
-      },
-      {
-        type: 'inside',
-        yAxisIndex: 0,
-        filterMode: 'empty'
-      }
-    ],
     series: [{
-      id: 'a',
-      type: 'line',
-      smooth: true,
-      symbolSize: symbolSize,
-      data: data
+      data: [120, 200, 150, 80, 70, 110, 130],
+      type: 'bar'
     }]
   };
-
-  chart.setOption(option);
-  return chart;
 }
 
 Page({
   data: {
     active: 0,
     ec: {
-      onInit: initChart
+      // 将 lazyLoad 设为 true 后，需要手动初始化图表
+      lazyLoad: true
     }
   },
 
-  onReady() {},
+  onReady() {
+    // 获取组件（父组件中获取其子组件，使用this.selectComponent()方法
+    this.ecComponent = this.selectComponent('#mychart-dom-bar');
+  },
+
   onChange(event) {
-    this.setData({
-      ec: {
-        onInit:initChart
-      }
-    })
-    wx.showToast({
-      title: `切换到标签 ${event.detail.index + 1}`,
-      icon: 'none'
+    this.ecComponent.init((canvas, width, height) => {
+      // 获取组件的 canvas、width、height 后的回调函数
+      // 在这里初始化图表
+      const chart = echarts.init(canvas, null, {
+        width: width,
+        height: height
+      });
+
+      // 导入配置
+      chart.setOption(genOption());
+
+      // 将图表实例绑定到 this 上，可以在其他成员函数（如 dispose）中访问
+      this.chart = chart;
+
+      // 注意这里一定要返回 chart 实例，否则会影响事件处理等
+      return chart;
     });
   },
 });
